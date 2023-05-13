cc7eace68a62227c8a2df0a03136ca7298d057d5
Alan Herculano Diniz <alanhdiniz@gmail.com>
2023-05-13 17:45:06 -0300

    Running the gmres experiment

--- gmres_experiment.m
+++ gmres_experiment.m
@@ -50,7 +50,6 @@ function gmres_experiment()
         # No preconditioning
         fprintf(data_file, "\nNo Preconditioning\n\n");
 
-        [noprecond_x, noprecond_flag, noprecond_relres, noprecond_iter, noprecond_resvec] = [zeros(n, 1), -1, .0, zeros(1, 2), zeros(n, 2)];
         timer = clock();
         if test_params(i)
             [noprecond_x, noprecond_flag, noprecond_relres, noprecond_iter, noprecond_resvec, used_k, used_tol, used_maxit] = test_gmres_params(A, b, ks{i}, tols{i}, maxit);
@@ -72,101 +71,125 @@ function gmres_experiment()
         # Only ILU(0) preconditioning
         fprintf(data_file, "\nILU(0) Preconditioning\n\n");
 
-        timer = clock();
-        opts.type = "nofill";
-        [L, U] = ilu(A, opts);
-        [ilu0_x, ilu0_flag, ilu0_relres, ilu0_iter, ilu0_resvec] = gmres(A, b, used_k, used_tol, used_maxit, L, U);
-        elapsed_time = etime(clock(), timer);
+        ilu0_iter_n = 0;
+        ilu0_resvec = [];
+        try
+            timer = clock();
+            opts.type = "nofill";
+            [L, U] = ilu(A, opts);
+            [ilu0_x, ilu0_flag, ilu0_relres, ilu0_iter, ilu0_resvec] = gmres(A, b, used_k, used_tol, used_maxit, L, U);
+            elapsed_time = etime(clock(), timer);
 
-        ilu0_iter_n = ilu0_iter(1, 1) * used_k + ilu0_iter(1, 2);
+            ilu0_iter_n = ilu0_iter(1, 1) * used_k + ilu0_iter(1, 2);
 
-        fprintf(data_file, "Convergence Flag: %d\n", ilu0_flag);
-        fprintf(data_file, "Iterations: %d\n", ilu0_iter_n);
-        fprintf(data_file, "Solution Inf. Norm: %e\n", norm(ilu0_x, inf));
-        fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
-        fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
+            fprintf(data_file, "Convergence Flag: %d\n", ilu0_flag);
+            fprintf(data_file, "Iterations: %d\n", ilu0_iter_n);
+            fprintf(data_file, "Solution Inf. Norm: %e\n", norm(ilu0_x, inf));
+            fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
+            fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
 
-        hf = figure();
-        spy(L);
-        print(hf, sprintf("out/%s_spy_L_ilu0.png", matrices{i}), "-dpng");
-        hf = figure();
-        spy(U);
-        print(hf, sprintf("out/%s_spy_U_ilu0.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(L);
+            print(hf, sprintf("out/%s_spy_L_ilu0.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(U);
+            print(hf, sprintf("out/%s_spy_U_ilu0.png", matrices{i}), "-dpng");
+        catch
+            fprintf(data_file, "\nERROR ILU(0) for %s\n", matrices{i});
+        end
         
         # ILU(0) preconditioning with line reordering
         fprintf(data_file, "\nILU(0) Preconditioning RCM\n\n");
 
-        timer = clock();
-        opts.type = "nofill";
-        [L, U] = ilu(R, opts);
-        [ilu0r_x, ilu0r_flag, ilu0r_relres, ilu0r_iter, ilu0r_resvec] = gmres(R, rb, used_k, used_tol, used_maxit, L, U);
-        elapsed_time = etime(clock(), timer);
+        ilu0r_iter_n = 0;
+        ilu0r_resvec = [];
+        try
+            timer = clock();
+            opts.type = "nofill";
+            [L, U] = ilu(R, opts);
+            [ilu0r_x, ilu0r_flag, ilu0r_relres, ilu0r_iter, ilu0r_resvec] = gmres(R, rb, used_k, used_tol, used_maxit, L, U);
+            elapsed_time = etime(clock(), timer);
 
-        ilu0r_iter_n = ilu0r_iter(1, 1) * used_k + ilu0r_iter(1, 2);
+            ilu0r_iter_n = ilu0r_iter(1, 1) * used_k + ilu0r_iter(1, 2);
 
-        fprintf(data_file, "Convergence Flag: %d\n", ilu0r_flag);
-        fprintf(data_file, "Iterations: %d\n", ilu0r_iter_n);
-        fprintf(data_file, "Solution Inf. Norm: %e\n", norm(ilu0r_x, inf));
-        fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
-        fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
+            fprintf(data_file, "Convergence Flag: %d\n", ilu0r_flag);
+            fprintf(data_file, "Iterations: %d\n", ilu0r_iter_n);
+            fprintf(data_file, "Solution Inf. Norm: %e\n", norm(ilu0r_x, inf));
+            fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
+            fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
 
-        hf = figure();
-        spy(L);
-        print(hf, sprintf("out/%s_spy_L_ilu0r.png", matrices{i}), "-dpng");
-        hf = figure();
-        spy(U);
-        print(hf, sprintf("out/%s_spy_U_ilu0r.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(L);
+            print(hf, sprintf("out/%s_spy_L_ilu0r.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(U);
+            print(hf, sprintf("out/%s_spy_U_ilu0r.png", matrices{i}), "-dpng");
+        catch
+            fprintf(data_file, "\nERROR ILU(0) RCM for %s\n", matrices{i});
+        end
 
         # Only ILU crout preconditioning
         fprintf(data_file, "\nILU Crout Preconditioning\n\n");
 
-        timer = clock();
-        opts.type = "crout";
-        opts.droptol = 1e-4;
-        [L, U] = ilu(A, opts);
-        [crout_x, crout_flag, crout_relres, crout_iter, crout_resvec] = gmres(A, b, used_k, used_tol, used_maxit, L, U);
-        elapsed_time = etime(clock(), timer);
+        crout_iter_n = 0;
+        crout_resvec = [];
+        try
+            timer = clock();
+            opts.type = "crout";
+            opts.droptol = 1e-4;
+            [L, U] = ilu(A, opts);
+            [crout_x, crout_flag, crout_relres, crout_iter, crout_resvec] = gmres(A, b, used_k, used_tol, used_maxit, L, U);
+            elapsed_time = etime(clock(), timer);
 
-        crout_iter_n = crout_iter(1, 1) * used_k + crout_iter(1, 2);
+            crout_iter_n = crout_iter(1, 1) * used_k + crout_iter(1, 2);
 
-        fprintf(data_file, "Convergence Flag: %d\n", crout_flag);
-        fprintf(data_file, "Iterations: %d\n", crout_iter_n);
-        fprintf(data_file, "Solution Inf. Norm: %e\n", norm(crout_x, inf));
-        fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
-        fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
+            fprintf(data_file, "Convergence Flag: %d\n", crout_flag);
+            fprintf(data_file, "Iterations: %d\n", crout_iter_n);
+            fprintf(data_file, "Solution Inf. Norm: %e\n", norm(crout_x, inf));
+            fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
+            fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
 
-        hf = figure();
-        spy(L);
-        print(hf, sprintf("out/%s_spy_L_crout.png", matrices{i}), "-dpng");
-        hf = figure();
-        spy(U);
-        print(hf, sprintf("out/%s_spy_U_crout.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(L);
+            print(hf, sprintf("out/%s_spy_L_crout.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(U);
+            print(hf, sprintf("out/%s_spy_U_crout.png", matrices{i}), "-dpng");
+        catch
+            fprintf(data_file, "\nERROR ILU Crout for %s\n", matrices{i});
+        end
 
         # ILU crout preconditioning with line reordering
         fprintf(data_file, "\nILU Crout Preconditioning RCM\n\n");
 
-        timer = clock();
-        opts.type = "crout";
-        opts.droptol = 1e-4;
-        [L, U] = ilu(R, opts);
-        [croutr_x, croutr_flag, croutr_relres, croutr_iter, croutr_resvec] = gmres(R, rb, used_k, used_tol, used_maxit, L, U);
-        elapsed_time = etime(clock(), timer);
+        croutr_iter_n = 0;
+        croutr_resvec = [];
+        try
+            timer = clock();
+            opts.type = "crout";
+            opts.droptol = 1e-4;
+            [L, U] = ilu(R, opts);
+            [croutr_x, croutr_flag, croutr_relres, croutr_iter, croutr_resvec] = gmres(R, rb, used_k, used_tol, used_maxit, L, U);
+            elapsed_time = etime(clock(), timer);
 
-        croutr_iter_n = croutr_iter(1, 1) * used_k + croutr_iter(1, 2);
+            croutr_iter_n = croutr_iter(1, 1) * used_k + croutr_iter(1, 2);
 
-        fprintf(data_file, "Convergence Flag: %d\n", croutr_flag);
-        fprintf(data_file, "Iterations: %d\n", croutr_iter_n);
-        fprintf(data_file, "Solution Inf. Norm: %e\n", norm(croutr_x, inf));
-        fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
-        fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
-        fclose(data_file);
+            fprintf(data_file, "Convergence Flag: %d\n", croutr_flag);
+            fprintf(data_file, "Iterations: %d\n", croutr_iter_n);
+            fprintf(data_file, "Solution Inf. Norm: %e\n", norm(croutr_x, inf));
+            fprintf(data_file, "nnz [L, U]: (%d, %d)\n", nnz(L), nnz(U));
+            fprintf(data_file, "Elapsed time: %ds\n", elapsed_time);
+            fclose(data_file);
 
-        hf = figure();
-        spy(L);
-        print(hf, sprintf("out/%s_spy_L_croutr.png", matrices{i}), "-dpng");
-        hf = figure();
-        spy(U);
-        print(hf, sprintf("out/%s_spy_U_croutr.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(L);
+            print(hf, sprintf("out/%s_spy_L_croutr.png", matrices{i}), "-dpng");
+            hf = figure();
+            spy(U);
+            print(hf, sprintf("out/%s_spy_U_croutr.png", matrices{i}), "-dpng");
+        catch
+            fprintf(data_file, "\nERROR ILU Crout RCM for %s\n", matrices{i});
+        end
 
         max_iter = max([noprecond_iter_n, ilu0_iter_n, ilu0r_iter_n, crout_iter_n, croutr_iter_n]);
 
